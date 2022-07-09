4046520833c9154d5e2655b712d5e7d9e4a33335
Carlos Gumucio Labbé <cgumuciolabbe@gmail.com>
2022-07-09 18:58:36 -0400

    initial setup dome

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import ShopLayout from "../components/layouts/ShopLayout";
 
 const Home: NextPage = () => {
-  return <h1>TESTLO SHOP</h1>;
+  return (
+    <ShopLayout
+      title={"Teslo Shop - Home"}
+      pageDescription={"Best Teslo products here!"}
+    >
+      <Typography variant='h1' component='h1'>
+        Home
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        All Products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;

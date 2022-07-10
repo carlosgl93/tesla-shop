14cf4652bbb0e43a4ff3b9a8dc4be7c61edf384a
Carlos Gumucio Labbé <cgumuciolabbe@gmail.com>
2022-07-09 22:19:16 -0400

    added animation and onhover state reactivity to productCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,14 @@
-import { Grid, Card, CardActionArea, CardMedia } from "@mui/material";
-import React, { FC } from "react";
+import React, { FC, useMemo, useState } from "react";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+
 import { IProduct } from "../../interfaces";
 
 interface Props {
@@ -7,17 +16,36 @@ interface Props {
 }
 
 const ProductCard: FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item key={product.slug} xs={6} sm={4}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
+            className='fadeIn'
             alt={product.title}
             component='img'
-            image={`products/${product.images[0]}`}
+            image={productImage}
           />
         </CardActionArea>
       </Card>
+      <Box sx={{ mt: 1 }} className='fadeIn'>
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+      </Box>
     </Grid>
   );
 };

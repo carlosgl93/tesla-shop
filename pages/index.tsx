6ecb963ccb256a7c8ae59b63ea4a5f98b7ba0aea
Carlos Gumucio Labbé <cgumuciolabbe@gmail.com>
2022-07-09 20:44:51 -0400

    added grid items to the index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import type { NextPage } from "next";
 import ShopLayout from "../components/layouts/ShopLayout";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +21,23 @@ const Home: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         All Products
       </Typography>
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => {
+          return (
+            <Grid item key={product.slug} xs={6} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    alt={product.title}
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </ShopLayout>
   );
 };
